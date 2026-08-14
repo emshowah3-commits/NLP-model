4d781a04cbbbaf224143b335dc4252ac2f612f63
Enk-sho portfolio <emshowah3@gmail.com>
2026-08-13 21:42:49 -0230

    Add files via upload

--- NLP_Model.py
+++ NLP_Model.py
@@ -3,5 +3,6 @@ customer_serv = ["This product was nice!", "This product was horrible"]
 key_review = ['nice', '!', 'horrible']
 
 for _ in key_review:
-    sum(key_review, customer_serv, )
+    Review = sum(1 for _ in key_review if _ in key_review)
 
+print(customer_serv, '-->', 'Positive' if Review > 0 else 'Needs review' )
